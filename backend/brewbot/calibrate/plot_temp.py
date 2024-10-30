5442debe741648b9320b5f070506041f5aa3b771
Simon Barthel <s.b4rth3l@gmail.com>
2024-10-30 18:40:23 +0100

    plots

--- backend/brewbot/calibrate/plot_temp.py
+++ backend/brewbot/calibrate/plot_temp.py
@@ -25,214 +25,9 @@ class RecGroup:
     slices: list[slice]
 
 
+def time_plot(recs, dfs, ax1):
 
-def main():
-    group_1k = RecGroup(
-        name="1k Amp",
-        file_paths = [
-            "recordings/rec_2024-10-20T16-14-16.txt",
-            "recordings/rec_2024-10-20T18-15-37.txt",
-            "recordings/rec_2024-10-20T20-15-19.txt",
-            "recordings/rec_2024-10-20T21-45-40.txt",
-            "recordings/rec_2024-10-21T11-11-13.txt",
-            "recordings/rec_2024-10-21T11-58-03.txt"
-        ],
-        slices=[
-
-        ]
-    )
-
-    group_2k = RecGroup(
-        name="2k no Amp",
-        file_paths = [
-            "recordings/rec_2024-10-21T14-39-31_filtered.txt",
-            "recordings/rec_2024-10-21T15-01-36.txt",
-            "recordings/rec_2024-10-21T15-56-51.txt",
-            "recordings/rec_2024-10-21T16-02-35.txt",
-            "recordings/rec_2024-10-21T16-58-37.txt"
-        ],
-        slices=[
-            slice(400, 6600),
-            slice(6800, 13800),
-            slice(14500, 21000)
-        ]
-    )
-
-    group_2_4k = RecGroup(
-        name="2.4k no Amp",
-        file_paths = [
-            "recordings/rec_2024-10-21T19-12-49.txt"
-        ],
-        slices=[
-            slice(700, 9500),
-            slice(9800, 23500)
-        ]
-    )
-
-    group_3k_noamp = RecGroup(
-        name="3k no Amp",
-        file_paths = [
-            "recordings/rec_2024-10-22T07-14-55.txt",
-            "recordings/rec_2024-10-22T09-02-08.txt"
-        ],
-        slices=[
-            slice(800, 13500),
-            slice(13600, 24500)
-        ]
-    )
-
-    group_3k_amp = RecGroup(
-        name="3k Amp",
-        file_paths = [
-            "recordings/rec_2024-10-22T22-17-20.txt"
-        ],
-        slices=[
-            slice(700, None)
-        ]
-    )
-
-    group_3k_amp2 = RecGroup(
-        name="3k Amp2",
-        file_paths = [
-            "recordings/rec_2024-10-23T08-30-40.txt"
-        ],
-        slices=[
-            slice(120, None)
-        ]
-    )
-
-    group_3k_amp3 = RecGroup(
-        name="3k Amp3",
-        file_paths = [
-            "recordings/rec_2024-10-23T11-16-41.txt"
-        ],
-        slices=[
-            slice(2300, None)
-        ]
-    )
-
-    group_3k_amp4 = RecGroup(
-        name="3k Amp4",
-        file_paths = [
-            "recordings/rec_2024-10-23T12-06-10.txt"
-        ],
-        slices=[
-            slice(80, 2500)
-        ]
-    )
-
-    group_3k_amp5 = RecGroup(
-        name="3k Amp5",
-        file_paths = [
-            "recordings/rec_2024-10-23T12-38-40.txt"
-        ],
-        slices=[
-            slice(None, 11500),
-            slice(12600, 29000),
-            slice(30700, 33600),
-            slice(35200, 37500),
-            slice(38500, 39800),
-            slice(42000, None)
-        ]
-    )
-
-    group_3k_amp6 = RecGroup(
-        name="3k Amp6",
-        file_paths = [
-            "recordings/rec_2024-10-25T20-04-38.txt",
-            "recordings/rec_2024-10-26T08-04-30.txt",
-            "recordings/rec_2024-10-26T11-09-31.txt"
-        ],
-        slices=[
-            slice(7050, 8000), # slice(7050, 23200),
-            # slice(23900, 25000), # slice(23900, 44500),
-            # slice(45300, 46000) # slice(45300, None)
-        ]
-    )
-
-    group_3k_amp7 = RecGroup(
-        name="3k Amp7",
-        file_paths = [
-            # "recordings/rec_2024-10-26T16-33-57.txt",
-            # "recordings/rec_2024-10-28T07-59-23.txt",
-            # "recordings/rec_2024-10-28T09-45-38.txt" # cap
-            # "recordings/rec_2024-10-28T11-38-45.txt", # cap container
-            "recordings/rec_2024-10-28T15-51-55.txt" # cap container 2
-            "recordings/rec_2024-10-29T09-13-42.txt" # cap container ice to room temp
-        ],
-        slices=[
-            # slice(3450, None) # cap
-            # slice(7900, None) # cap
-            # slice(800, 29100), # cap container
-            # slice(29200, None) # cap container 2
-            slice(4000, None)
-        ]
-    )
-
-    group_3k_amp7_oil = RecGroup(
-        name="3k Amp7 Oil",
-        file_paths = [
-            "recordings/rec_2024-10-26T20-16-33.txt"
-        ],
-        slices=[
-            slice(1000, None)
-        ]
-    )
-
-    group_3k_amp8_cap = RecGroup(
-        name="3k Amp8 Cap",
-        file_paths = [
-            "recordings/rec_2024-10-26T21-55-29.txt"
-        ],
-        slices=[
-            slice(600, None)
-        ]
-    )
-
-    recs = [
-        RecSequence(
-            file_path="recordings/rec_2024-10-28T15-51-55.txt",
-            slc=slice(100, 42000),
-            temp_spline_divider=10,
-            voltage_filter_sigma=20
-        ),
-        RecSequence(
-            file_path="recordings/rec_2024-10-29T09-13-42.txt",
-            slc=slice(4000, None),
-            temp_spline_divider=10,
-            voltage_filter_sigma=750.0
-        )
-    ]
-
-    group = group_3k_amp7
-
-    # show_plot = "temp_to_v"
-    # show_plot = "time"
-    show_plot = "spline"
-
-    v_factor = 5.033 / 5.0  # real voltage measured at can module / max voltage assumed in can message
-
-    dfs = []
-    for rec in recs:
-        with open(rec.file_path) as f:
-            df = pd.DataFrame([json.loads(line) for line in f][rec.slc])
-            df['time'] = pd.to_datetime(df['time'], errors='coerce')
-            df = df.astype({"real_temp_c": "float64", "can_temp_v": "float64"})
-            df = df.set_index("time")
-            df.loc[(df['real_temp_c'] < 1), 'real_temp_c'] = df['real_temp_c'] + 100
-            df['can_temp_v'] = df['can_temp_v'] * v_factor
-            df['real_temp_c_smoothed'] = df['real_temp_c'].rolling(window=5, center=True).median()
-            df['real_temp_c_interpolated'] = df['real_temp_c_smoothed'].interpolate(method='linear')
-            df['can_temp_v_smoothed'] = df['can_temp_v'].rolling(window=5, center=True).median()
-            df = df.iloc[2:-2]
-
-            dfs.append(df)
-
-    fig, ax1 = plt.subplots(figsize=(10, 6))
-
-    if show_plot == "time":
-        df = dfs[0]
-
+    for df in dfs:
         # Plot real_temp_c on the left y-axis
         ax1.plot(df.index, df['real_temp_c_smoothed'], color='blue', label='Real Temp (C)')
         ax1.set_xlabel('Time')
@@ -245,10 +40,11 @@ def main():
         ax2.set_ylabel('CAN Temp (V)', color='red')
         ax2.tick_params(axis='y', labelcolor='red')
 
-        plt.xticks(rotation=45)
+    plt.xticks(rotation=45)
 
-        plt.title('Temperature Data Over Time')
-    elif show_plot == "temp_to_v":
+
+def temp_to_v_means_plot(recs, dfs, ax1):
+    for df in dfs:
         grouped = df.groupby('real_temp_c_smoothed')['can_temp_v_smoothed']
         box_plot_data = [grouped.get_group(temp) for temp in grouped.groups]
         means = grouped.mean()
@@ -290,77 +86,130 @@ def main():
         # ax1.set_ylabel('CAN Temp (V)')
         # ax1.tick_params(axis='y', labelcolor='red')
 
-        plt.title(f"Temperature To Voltage {group.name}")
-    elif show_plot == "spline":
-        time_list = []
-        time_smooth_list = []
-        temps_list = []
-        temps_smooth_list = []
-        temps_bin_list = []
-        voltages_list = []
-        voltages_smooth_list = []
-        voltages_bin_list = []
 
-        for rec, df in zip(recs, dfs):
-            time_start = df.index[0]
-            df['time_sec'] = (df.index - time_start).total_seconds()
+def smoothed_plot(recs, dfs, ax1):
+    time_list = []
+    time_smooth_list = []
+    temps_list = []
+    temps_smooth_list = []
+    temps_bin_list = []
+    voltages_list = []
+    voltages_smooth_list = []
+    voltages_bin_list = []
 
-            time = df['time_sec'].values
-            temps = df['real_temp_c_interpolated'].values
-            voltage = df['can_temp_v_smoothed'].values
+    for rec, df in zip(recs, dfs):
+        time_start = df.index[0]
+        df['time_sec'] = (df.index - time_start).total_seconds()
 
-            time_smooth = np.linspace(time.min(), time.max(),  len(time))
-            temp_spline = UnivariateSpline(time, temps, s=len(time)//rec.temp_spline_divider)
+        time = df['time_sec'].values
+        temps = df['real_temp_c_interpolated'].values
+        voltage = df['can_temp_v_smoothed'].values
 
-            temps_smooth = temp_spline(time_smooth)
-            voltages_smooth = gaussian_filter1d(voltage, sigma=rec.voltage_filter_sigma)
+        time_smooth = np.linspace(time.min(), time.max(),  len(time))
+        temp_spline = UnivariateSpline(time, temps, s=len(time)//rec.temp_spline_divider)
 
-            volt_to_temp_df = pd.DataFrame({'voltage': voltages_smooth, 'temp': temps_smooth})
-            bin_width = 0.1
-            temp_bins = np.arange(volt_to_temp_df['temp'].min(), volt_to_temp_df['temp'].max() + bin_width, bin_width)
-            volt_to_temp_df['temp_bin'] = pd.cut(volt_to_temp_df['temp'], temp_bins)
-            volt_to_temp_binned_groups = volt_to_temp_df.groupby('temp_bin', observed=True).agg({'temp': 'median', 'voltage': 'median'}).dropna()
+        temps_smooth = temp_spline(time_smooth)
+        voltages_smooth = gaussian_filter1d(voltage, sigma=rec.voltage_filter_sigma)
 
-            time_list.append(time)
-            time_smooth_list.append(time_smooth)
-            temps_list.append(temps)
-            temps_smooth_list.append(temps_smooth)
-            temps_bin_list.append(volt_to_temp_binned_groups['temp'])
-            voltages_list.append(voltage)
-            voltages_smooth_list.append(voltages_smooth)
-            voltages_bin_list.append(volt_to_temp_binned_groups['voltage'])
+        volt_to_temp_df = pd.DataFrame({'voltage': voltages_smooth, 'temp': temps_smooth})
+        bin_width = 0.1
+        temp_bins = np.arange(volt_to_temp_df['temp'].min(), volt_to_temp_df['temp'].max() + bin_width, bin_width)
+        volt_to_temp_df['temp_bin'] = pd.cut(volt_to_temp_df['temp'], temp_bins)
+        volt_to_temp_binned_groups = volt_to_temp_df.groupby('temp_bin', observed=True).agg({'temp': 'median', 'voltage': 'median'}).dropna()
 
-        times = np.hstack(time_list)
-        times_smooth = np.hstack(time_smooth_list)
-        temps = np.hstack(temps_list)
-        temps_smooth = np.hstack(temps_smooth_list)
-        temps_bins = np.hstack(temps_bin_list)
-        voltages = np.hstack(voltages_list)
-        voltages_smooth = np.hstack(voltages_smooth_list)
-        voltages_bins = np.hstack(voltages_bin_list)
+        time_list.append(time)
+        time_smooth_list.append(time_smooth)
+        temps_list.append(temps)
+        temps_smooth_list.append(temps_smooth)
+        temps_bin_list.append(volt_to_temp_binned_groups['temp'])
+        voltages_list.append(voltage)
+        voltages_smooth_list.append(voltages_smooth)
+        voltages_bin_list.append(volt_to_temp_binned_groups['voltage'])
 
-        poly = np.polyfit(voltages_bins, temps_bins, deg=2)
-        print(poly)
+    times = np.hstack(time_list)
+    times_smooth = np.hstack(time_smooth_list)
+    temps = np.hstack(temps_list)
+    temps_smooth = np.hstack(temps_smooth_list)
+    temps_bins = np.hstack(temps_bin_list)
+    voltages = np.hstack(voltages_list)
+    voltages_smooth = np.hstack(voltages_smooth_list)
+    voltages_bins = np.hstack(voltages_bin_list)
 
-        vs = np.linspace(np.min(voltages_bins), np.max(voltages_bins))
-        ts = np.polyval(poly, vs)
+    poly = np.polyfit(voltages_bins, temps_bins, deg=2)
+    print(poly)
 
-        ax1.scatter(voltages_bins, temps_bins, color="blue")
-        ax1.plot(vs, ts, color="red")
+    # vs = np.linspace(np.min(voltages_bins), np.max(voltages_bins))
+    # ts = np.polyval(poly, vs)
 
-        # ax1.plot(times, temps, color='blue') # color='skyblue')
-        # ax1.plot(times_smooth, temps_smooth, color="blue")
-        # ax1.set_xlabel('Time (s)')
-        # ax1.set_ylabel('Real Temp (C)', color='blue')
-        # ax1.tick_params(axis='y', labelcolor='blue')
+    # ax1.scatter(voltages_bins, temps_bins, color="blue")
+    # ax1.plot(vs, ts, color="red")
 
-        # ax2 = ax1.twinx()
-        # ax2.plot(times, voltages, color='red') # color='pink')
-        # ax2.plot(times_smooth, voltages_smooth, color='red')
-        # ax2.set_ylabel('CAN Temp (V)', color='red')
-        # ax2.tick_params(axis='y', labelcolor='red')
+    ax1.plot(times, temps, color='skyblue')
+    ax1.plot(times_smooth, temps_smooth, color="blue")
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Real Temp (C)', color='blue')
+    ax1.tick_params(axis='y', labelcolor='blue')
 
-        # plt.xticks(rotation=45)
+    ax2 = ax1.twinx()
+    ax2.plot(times, voltages, color='pink')
+    ax2.plot(times_smooth, voltages_smooth, color='red')
+    ax2.set_ylabel('CAN Temp (V)', color='red')
+    ax2.tick_params(axis='y', labelcolor='red')
+
+    plt.xticks(rotation=45)
+
+
+def main():
+    group_name = "3k OP Amp"
+    recs = [
+        RecSequence(
+            file_path="recordings/rec_2024-10-28T15-51-55.txt",
+            slc=slice(100, 42000),
+            temp_spline_divider=10,
+            voltage_filter_sigma=20
+        ),
+        RecSequence(
+            file_path="recordings/rec_2024-10-29T09-13-42.txt",
+            slc=slice(4000, None),
+            temp_spline_divider=10,
+            voltage_filter_sigma=750.0
+        )
+    ]
+
+    # show_plot = "temp_to_v"
+    # show_plot = "time"
+    show_plot = "smoothed"
+
+    v_factor = 5.033 / 5.0  # real voltage measured at can module / max voltage assumed in can message
+    rolling_window = 5
+
+    dfs = []
+    for rec in recs:
+        with open(rec.file_path) as f:
+            df = pd.DataFrame([json.loads(line) for line in f][rec.slc])
+            df['time'] = pd.to_datetime(df['time'], errors='coerce')
+            df = df.astype({"real_temp_c": "float64", "can_temp_v": "float64"})
+            df = df.set_index("time")
+            df.loc[(df['real_temp_c'] < 1), 'real_temp_c'] = df['real_temp_c'] + 100
+            df['can_temp_v'] = df['can_temp_v'] * v_factor
+            df['real_temp_c_smoothed'] = df['real_temp_c'].rolling(window=rolling_window, center=True).median()
+            df['real_temp_c_interpolated'] = df['real_temp_c_smoothed'].interpolate(method='linear')
+            df['can_temp_v_smoothed'] = df['can_temp_v'].rolling(window=rolling_window, center=True).median()
+            df = df.iloc[(rolling_window//2):-(rolling_window//2)]
+
+            dfs.append(df)
+
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    if show_plot == "time":
+        plt.title('Temperature Data Over Time')
+        time_plot(recs, dfs, ax1)
+    elif show_plot == "temp_to_v":
+        plt.title(f"Temperature To Voltage {group_name}")
+        temp_to_v_means_plot(recs, dfs, ax1)
+    elif show_plot == "smoothed":
+        plt.title(f"Temperature To Voltage Smoothed {group_name}")
+        smoothed_plot(recs, dfs, ax1)
 
     plt.tight_layout()
     plt.savefig('temp_plot.png')
